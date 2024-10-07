2b71b02857fe65c80ad6bec8e9552bb3aefdf85b
thebestlearner-ric <watto1990@gmail.com>
2024-10-08 00:03:16 +0800

    change in vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,19 +1,9 @@
+# VPC Definition
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "private_subnet_a" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-west-2a"
-}
-
-resource "aws_subnet" "private_subnet_b" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-west-2b"
-}
-
+# Public Subnets
 resource "aws_subnet" "public_subnet_a" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
@@ -28,15 +18,88 @@ resource "aws_subnet" "public_subnet_b" {
   map_public_ip_on_launch = true
 }
 
+# Private Subnets
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2a"
+}
 
+resource "aws_subnet" "private_subnet_b" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-west-2b"
+}
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_a.id  # Place the NAT gateway in a public subnet
+}
+
+# Public Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id  # Public route to the internet
+  }
+}
+
+# Associate Public Route Table with Public Subnets
+resource "aws_route_table_association" "public_route_table_assoc_a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_route_table_assoc_b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id  # Private route through the NAT gateway
+  }
+}
+
+# Associate Private Route Table with Private Subnets
+resource "aws_route_table_association" "private_route_table_assoc_a" {
+  subnet_id      = aws_subnet.private_subnet_a.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_route_table_assoc_b" {
+  subnet_id      = aws_subnet.private_subnet_b.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+# Internet Gateway for the public subnets
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+# Security Group for EKS
 resource "aws_security_group" "eks_security_group" {
   vpc_id = aws_vpc.main_vpc.id
+
   ingress {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 65535
